48a931e0bf40014bb16b46efe40b075a0931f98c
adarsshkg-codebank <akgcodebank@gmail.com>
2025-06-21 21:10:19 +0530

    better lifetime and error handling

--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,11 +1,14 @@
 use core::fmt::Display;
 use crossterm::cursor::{Hide, MoveTo, Show};
 use crossterm::style::Print;
-use crossterm::terminal::{Clear, ClearType, disable_raw_mode, enable_raw_mode, size};
+use crossterm::terminal::{
+    Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode,
+    enable_raw_mode, size,
+};
 use crossterm::{Command, queue};
 use std::io::{Error, Write, stdout};
 
-#[derive(Clone, Copy)]
+#[derive(Default, Clone, Copy)]
 pub struct Size {
     pub height: usize,
     pub width: usize,
@@ -22,12 +25,15 @@ pub struct Terminal;
 impl Terminal {
     pub fn initialize() -> Result<(), Error> {
         enable_raw_mode()?;
+        Self::enter_alternate_screen()?;
         Self::clear_screen()?;
         Self::execute()?;
         Ok(())
     }
 
     pub fn terminate() -> Result<(), Error> {
+        Self::leave_alternate_screen()?;
+        Self::show_caret()?;
         Self::execute()?;
         disable_raw_mode()?;
         Ok(())
@@ -77,6 +83,23 @@ impl Terminal {
 
     fn queue_command<T: Command>(command: T) -> Result<(), Error> {
         queue!(stdout(), command)?;
+        Ok(())
+    }
+
+    pub fn enter_alternate_screen() -> Result<(), Error> {
+        Self::queue_command(EnterAlternateScreen)?;
+        Ok(())
+    }
+
+    pub fn leave_alternate_screen() -> Result<(), Error> {
+        Self::queue_command(LeaveAlternateScreen)?;
+        Ok(())
+    }
+
+    pub fn print_row(row: usize, line_text: &str) -> Result<(), Error> {
+        Self::move_caret_to(Position { col: 0, row })?;
+        Self::clear_line()?;
+        Self::print(line_text)?;
         Ok(())
     }
 }
--- src/editor/view.rs
+++ src/editor/view.rs
@@ -7,71 +7,75 @@ use buffer::Buffer;
 const NAME: &str = env!("CARGO_PKG_NAME");
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
-#[derive(Default)]
 pub struct View {
     buffer: Buffer,
+    size: Size,
+    needs_redraw: bool,
+}
+
+impl Default for View {
+    fn default() -> Self {
+        Self {
+            buffer: Buffer::default(),
+            needs_redraw: true,
+            size: Terminal::size().unwrap_or_default(),
+        }
+    }
 }
 
 impl View {
-    fn draw_welcome_message() -> Result<(), Error> {
-        let mut welcome = format!("{NAME} -- version {VERSION}");
-        let width = Terminal::size()?.width;
-        let len = welcome.len();
-        let padding = (width.saturating_sub(len)) / 2;
-        let spaces = " ".repeat(padding.saturating_sub(1));
-        welcome = format!("~{spaces}{welcome}");
-        welcome.truncate(width);
-        Terminal::print(welcome)?;
-        Ok(())
+    pub fn resize(&mut self, to: Size) {
+        self.size = to;
+        self.needs_redraw = true;
     }
 
-    fn draw_empty_row() -> Result<(), Error> {
-        Terminal::print("~")?;
-        Ok(())
+    fn render_line(at: usize, line_text: &str) {
+        let result = Terminal::print_row(at, line_text);
+        debug_assert!(result.is_ok(), "Failes to render line");
     }
 
-    pub fn render_welcome_screen() -> Result<(), Error> {
-        let Size { height, .. } = Terminal::size()?;
-        for current_row in 0..height {
-            Terminal::clear_line()?;
-
-            if current_row == height / 3 {
-                Self::draw_welcome_message()?;
-            } else {
-                Self::draw_empty_row()?;
-            }
-            if current_row.saturating_add(1) < height {
-                Terminal::print("\r\n")?;
-            }
+    pub fn render(&mut self) {
+        if !self.needs_redraw {
+            return;
         }
-        Ok(())
-    }
-
-    pub fn render_buffer(&self) -> Result<(), Error> {
-        let Size { height, .. } = Terminal::size()?;
+        let Size { height, width } = self.size;
+        if height == 0 || width == 0 {
+            return;
+        }
+        let vertical_center = height / 3;
 
         for current_row in 0..height {
-            Terminal::clear_line()?;
             if let Some(line) = self.buffer.lines.get(current_row) {
-                Terminal::print(line)?;
-                Terminal::print("\r\n")?;
+                let truncated_line = if line.len() >= width {
+                    &line[0..width]
+                } else {
+                    line
+                };
+                Self::render_line(current_row, truncated_line);
+            } else if current_row == vertical_center && self.buffer.is_empty() {
+                Self::render_line(current_row, &Self::build_welcome_message(width));
             } else {
-                Self::draw_empty_row()?;
-                if current_row.saturating_add(1) < height {
-                    Terminal::print("\r\n")?;
-                }
+                Self::render_line(current_row, "~");
             }
         }
-        Ok(())
+        self.needs_redraw = false;
     }
 
-    pub fn render(&self) -> Result<(), Error> {
-        if self.buffer.is_empty() {
-            Self::render_welcome_screen()?;
-        } else {
-            self.render_buffer()?;
+    fn build_welcome_message(width: usize) -> String {
+        if width == 0 {
+            return " ".to_string();
         }
-        Ok(())
+        let welcome_message = format!("{NAME} editor -- version {VERSION}");
+        let len = welcome_message.len();
+        if width <= len {
+            return "~".to_string();
+        }
+
+        let padding = (width.saturating_sub(len).saturating_sub(1)) / 2;
+
+        let mut full_message = format!("~{}{}", " ".repeat(padding), welcome_message);
+        full_message.truncate(width);
+        full_message
     }
 
     pub fn load(&mut self, file_name: &str) {
